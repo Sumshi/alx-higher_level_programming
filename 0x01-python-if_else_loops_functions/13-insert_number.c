936cf57c963eef76f58d47610428f1b48182a40a
Sumshi <sumayasomow@gmail.com>
2023-06-06 14:27:44 +0000

    betty fixed

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,25 +10,26 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *current;
+	listint_t *new;/*stores new created node*/
+	listint_t *current;/*for traversing the list*/
 
-	new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));/*allocates memory for new node*/
 	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
-
+		return (NULL);/*memory allocation failed*/
+	new->n = number;/*stores the number*/
+	new->next = NULL;/*there is no node after it*/
+	/*linked list is empty or number to insert is less than num current node*/
 	if (*head == NULL || number < (*head)->n)
 	{
-		new->next = *head;
+		new->next = *head;/*next pointer is set to current head*/
 		*head = new;
 		return (new);
 	}
-	current = *head;
+	current = *head;/*traverses linked list till position to insert*/
 	while (current->next != NULL && current->next->n < number)
 		current = current->next;
 
-	new->next = current->next;
+	new->next = current->next;/*new node is inserted*/
 	current->next = new;
 
 	return (new);
