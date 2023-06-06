4326e23b6734a60abb0c49a575ed0c87ec40bbda
Sumshi <sumayasomow@gmail.com>
2023-06-06 18:56:39 +0000

    small fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for x in range(10):
     for y in range(x + 1, 10):  # ensures second digit is always greater
         if x == 8 and y == 9:
-            print('89')
+            print('89')  # last number
         else:
             print('{}{}, '.format(x, y), end='')
