666f1903e3ea755c0d862372d6c009d05d06ff63
Sumshi <sumayasomow@gmail.com>
2023-06-06 10:48:38 +0000

    ascii values

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # ascii values for a-z
-    print("{}".format(chr(i)), end="")  # chr converts numbers to a -z
+    print('{}'.format(chr(i)), end='')  # chr converts numbers to a -z
     # end used to remove new line
