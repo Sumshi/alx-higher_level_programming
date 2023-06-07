6a95de6e00550cc1ab98591954e2b73eb542d42c
Sumshi <sumayasomow@gmail.com>
2023-06-07 11:31:14 +0000

    checks for a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,15 +10,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;/*Initialize slow pointer to the head of the list*/
-	listint_t *fast = list;/*Initialize fast pointer to the head of the list*/
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
 
 	/*Check if the list is null or contains only one node*/
 	if (list == NULL || list->next == NULL)
 		return (0);
-
+	slow = list;/*points to the first node*/
+	fast = list->next;/*points to second node*/
 	/*Traverse the list using slow and fast pointers*/
-	while (fast && fast->next)
+	while (fast != NULL && fast->next)
 	{
 		slow = slow->next;/*Move slow pointer by one step*/
 		fast = fast->next->next;/*Move fast pointer by two steps*/
