c8ef3f8094669e7748bf4d400f63716e81b9aca5
Sumshi <sumayasomow@gmail.com>
2023-06-08 09:10:13 +0000

    adds two numbers

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
+
+if __name__ == "__main__":  # will be executed if script is run directly
+    from add_0 import add
 a = 1
 b = 2
 # if __name__ == "__main__":
