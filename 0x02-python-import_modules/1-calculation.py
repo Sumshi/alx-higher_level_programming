a314cb069f387aee5ddac2587fac214d9e9bab77
Sumshi <sumayasomow@gmail.com>
2023-06-08 09:14:59 +0000

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":  # will be executed if script is run directly
+if __name__ == "__main__":  # will be executed if script is the main module
     from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
