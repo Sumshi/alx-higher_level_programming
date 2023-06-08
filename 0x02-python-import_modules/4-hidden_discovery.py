863c3e2daf2774a3b83b591e9e1e13c44296a338
Sumshi <sumayasomow@gmail.com>
2023-06-08 09:35:13 +0000

    hidden file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
+    import hidden_4  # imports hidden_4 in the same directory
     names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    for name in sorted(names):
+    for name in sorted(names):  # sort in alphabeticall order
         print(name)
