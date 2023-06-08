4777762a198bd3592d39a89376678f3d48f2113e
Sumshi <sumayasomow@gmail.com>
2023-06-08 09:44:46 +0000

    prints all names that do not start with __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4  # imports hidden_4 in the same directory
-    names = [name for name in dir(hidden_4) if not name.startswith('__')]
-    for name in sorted(names):  # sort in alphabeticall order
-        print(name)
+names = dir(hidden_4)  # displays all names in dir hidden_4
+for name in names:  # iterates over the list
+    if name[:2] != "__":  # name does not start with --
+        print(name)  # prints all names that do not start with --
