d5ef1d0782d9a8b1c2cfd1e74307f3b8b589eb50
Sumshi <sumayasomow@gmail.com>
2023-06-08 14:06:34 +0000

    prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4  # imports hidden_4 in the same directory
 names = dir(hidden_4)  # displays all names in dir hidden_4
 for name in names:  # iterates over the list
-    if name[:2] != "__":  # name does not start with --
+    if name[:2] != "__":  # name does not start with - -
         print(name)  # prints all names that do not start with --
