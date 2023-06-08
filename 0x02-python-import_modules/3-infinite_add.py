edcde4c2616c636973807242345ffae3803d0d67
Sumshi <sumayasomow@gmail.com>
2023-06-08 13:56:16 +0000

    almost done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 total = 0
 args = sys.argv
 if len(args) > 1:  # excludes program name
-    for arg in sys.argv[1:]:
+    for arg in sys.argv[1:]:  # starts from second argument
         total = total + int(arg)
 print(total)
