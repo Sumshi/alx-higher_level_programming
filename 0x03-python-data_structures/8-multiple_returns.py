d802cdbe657a258a450ff988c38ad42f39eb1ee7
Sumshi <sumayasomow@gmail.com>
2023-06-10 20:23:02 +0000

    editted file 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)  # gets length of sentense
-    if length > 0:
+    if length == 0:  # length is empty if length == 0
+        first_char = None
+    else:  # length is not empty
         first_char = sentence[0]
-    else:
-        first_char = "None"
     return (length, first_char)
