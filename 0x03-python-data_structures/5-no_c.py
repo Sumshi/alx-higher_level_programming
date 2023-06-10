b2288f3f9b3e3f8abce00a28e1d99f545e55f2a9
Sumshi <sumayasomow@gmail.com>
2023-06-10 19:31:47 +0000

    made small edits

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = ''  # empty string
+    new_string = ''  # empty string
     for char in my_string:
         if char != 'c' and char != 'C':
-            result = result + char
-    return result
+            new_string = new_string + char
+# each letter is added to the new string except cC
+    return new_string
