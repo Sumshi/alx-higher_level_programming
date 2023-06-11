b54b657ee430b90f3c1db44811f381637740217c
Sumshi <sumayasomow@gmail.com>
2023-06-11 13:35:50 +0000

    tests

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
+    for i in my_list:  # iterates through list while printing
         print("{:d}".format(i))  # d because it is an integer
