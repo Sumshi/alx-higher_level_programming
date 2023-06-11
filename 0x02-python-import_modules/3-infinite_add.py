23a4e8dc0816c05dbafe8ae05efacc4d3d6320cc
Sumshi <sumayasomow@gmail.com>
2023-06-11 21:06:11 +0000

    adds sum of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-total = 0
-args = sys.argv
-if len(args) > 1:  # excludes program name
-    for arg in sys.argv[1:]:  # starts from second argument
-        total = total + int(arg)
-print(total)
+sum = 0
+for num in sys.argv:
+    if num != sys.argv[0]:  # excludes program name
+        sum = sum + int(num)
+print(sum)
