46a4190c80801c52137c9edf85ee5bd2589564df
Sumshi <sumayasomow@gmail.com>
2023-06-14 11:20:18 +0000

    returns the weighted average of all integers tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,12 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    products = list(map(lambda x: x[0] * x[1], my_list))  # score * weight
-    weights = list(map(lambda x: x[1], my_list))  # gets weight only
-    return sum(products) / sum(weights)
+    total_score = 0
+    total_weight = 0
+    for tup in my_list:
+        total_score = total_score + tup[0] * tup[1]  # score * weight
+        total_weight = total_weight + tup[1]
+    return total_score / total_weight
+    # products = list(map(lambda x: x[0] * x[1], my_list))  # score * weight
+    # weights = list(map(lambda x: x[1], my_list))  # gets weight only
+    # return sum(products) / sum(weights)
