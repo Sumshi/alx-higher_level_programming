1e94d2ef9017cdedb950491458c15b3658c39638
Sumshi <sumayasomow@gmail.com>
2023-06-14 13:31:44 +0000

    edited task 0 code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,7 @@ def square_matrix_simple(matrix):
     for row in matrix:  # iterates over row
         new_row = []  # stores the square of rows
         for num in row:  # gets the square of each number
-            new_row.append(num ** 2)  # puts the resukt in new row
+            new_row.append(num ** 2)  # puts the result in new row
         new_matrix.append(new_row)  # new matrix contains squared nums
     return new_matrix
+    # return list([num * num for num in row] for row in matrix)
