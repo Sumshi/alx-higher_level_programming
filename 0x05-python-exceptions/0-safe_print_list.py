591bdbae68b9f678cb4e4a32c6379b1b305e8a97
Sumshi <sumayasomow@gmail.com>
2023-06-26 19:57:41 +0000

    edits

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             element = element + 1
         except IndexError:  # index out of range error
-            break
+            continue
     print("")  # prints new line
     return element
