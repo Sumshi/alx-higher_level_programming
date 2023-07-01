1e713db9c492c7b0ea680b0e39ce6e726a8ceb0b
Sumshi <sumayasomow@gmail.com>
2023-07-01 19:23:54 +0000

    some tests are failing

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,7 +21,7 @@ def matrix_divided(matrix, div):
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if type(div) not in (int, float):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
@@ -29,6 +29,6 @@ def matrix_divided(matrix, div):
 
     new_matrix = []
     for row in matrix:
-        new_row = [round(element / div, 2) for element in row]
-        new_matrix.append(new_row)
+            new_row = [round(element / div, 2) for element in row]
+            new_matrix.append(new_row)
     return new_matrix
