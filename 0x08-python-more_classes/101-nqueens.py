1516f2b49a1e0e1bcfe428e0f0bdbc92b39389f4
Sumshi <sumayasomow@gmail.com>
2023-07-04 06:44:01 +0000

    n queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,148 +1,57 @@
 #!/usr/bin/python3
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens
-on an N×N chessboard. Write a program that solves the N queens
-"""
-import sys
+from sys import argv
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-try:
-    N = sys.argv[1]
-    N = int(N)
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
-def generate_matrix(N):
+def check_if_safe(i, j, board):
     """
-    Generates a square matrix with "." values.
-
     Args:
-        N (int): The size of the square matrix.
+        i : Argument
+        j : Argument
+        board : Argument
 
-    Returns:
-        list: A square matrix with dimensions `size x size`
-        filled with "." values.
     """
-    board = [["." for row in range(N)] for column in range(N)]
-    return board
-
-
-def create_list(board, N):
-    """
-    Creates a List from the Matrix
-
-    Args:
-        board: Matrix
-        N: N
-
-    Returns:
-        list: list of positions
-    """
-    matrix = []
-    for row in range(N):
-        for column in range(N):
-            if board[row][column] == "X":
-                matrix.append([row, column])
-    return matrix
-
-
-def add_to_results(board, results_list, N):
-    """
-    Add solution to results
-
-    Args:
-        board (list): board
-        results_list (list): list of results
-        N (int): N
-    """
-    temp = []
-    for row in range(N):
-        string = ""
-        for column in range(N):
-            string += board[row][column]
-        temp.append(string)
-    results_list.append(temp)
-
-
-def is_safe(row, column, board, N):
-    """
-    Checks If addition is safe
-
-    Args:
-        row (int): row
-        column (int): column
-        board (list): board
-        N (int): N
-
-    Returns:
-        bool: True if safe False if not
-    """
-    x = row
-    y = column
-
-    while x >= 0:
-        if board[x][y] == "X":
+    for element in board:
+        if (
+            i == element[0]
+            or j == element[1]
+            or abs(i - element[0]) == abs(j - element[1])
+        ):
             return False
-        else:
-            x -= 1
-
-    x = row
-    y = column
-    while (y < N and x >= 0):
-        if board[x][y] == "X":
-            return False
-        else:
-            y += 1
-            x -= 1
-
-    x = row
-    y = column
-    while (y >= 0 and x >= 0):
-        if board[x][y] == "X":
-            return False
-        else:
-            x -= 1
-            y -= 1
     return True
 
 
-def NQueens(row, results_list, board, N):
+def backtrack(n, row, q_board, s_board):
     """
-    Recursive function to solve NQueens
-
     Args:
-        row (int): row
-        results_list (list): results_list
-        board (list): board
-        N (int): N
+        n : Argument
+        row : Argument
+        q_board : Argument
+        s_board : Argument
+
     """
-    if row == N:
-        add_to_results(board, results_list, N)
+    if row == n:
+        s_board.append(q_board.copy())
         return
 
-    for column in range(N):
-        if is_safe(row, column, board, N):
-            board[row][column] = "X"
-            NQueens(row + 1, results_list, board, N)
-            board[row][column] = "."
+    for column in range(n):
+        if check_if_safe(row, column, q_board):
+            q_board.append([row, column])
+            backtrack(n, row + 1, q_board, s_board)
+            q_board.pop()
 
 
-if __name__ == "__main__":
-
-    board = generate_matrix(N)
-
-    results_list = []
-    NQueens(0, results_list, board, N)
-
-    if len(results_list) != 0:
-        for result in results_list:
-            print(create_list(result, N))
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if not argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+n = int(argv[1])
+if n < 4:
+    print("N must be at least 4")
+    exit(1)
+solution_board = []
+queen_board = []
+backtrack(n, 0, queen_board, solution_board)
+for row in solution_board:
+    print(row)
