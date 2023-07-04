006ba4867609e0789c46e46a1b4b4708fc504a2d
Sumshi <sumayasomow@gmail.com>
2023-07-04 11:06:18 +0000

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""creates a class with no class or object attribute"""
+
+
 class LockedClass:
+    """slot is used to restrict the creation of instance attributes"""
     __slots__ = ['first_name']
