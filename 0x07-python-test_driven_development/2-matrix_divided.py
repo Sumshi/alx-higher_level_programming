d8ca2dae1e6c24311e34925307206a837c435d28
Sumshi <sumayasomow@gmail.com>
2023-07-05 18:09:11 +0000

    tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
-"""
-Module to divide a matrix
-"""
+
+"""This function divides all elements of a matrix"""
 
 
 def matrix_divided(matrix, div):
     """
-    A function that divides all elements of a matrix.
-
     Args:
-        matrix : Argument
-        div : Argument
+    matrix: a list of lists of integers or floats
+    div: a number(integer or float)
 
+    Return: a new matrix
     """
-    ErrMsg = "matrix must be a matrix (list of lists) of integers/floats"
-    ErrMsg2 = "Each row of the matrix must have the same size"
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise ValueError(error)
+
+    if any(type(row) is not list for row in matrix):
+        raise ValueError(error)
+
+    if any(len(row) != len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) is list:
-        if len(matrix) == 0:
-            raise TypeError(ErrMsg)
-        for row in matrix:
-            if type(row) is not list:
-                raise TypeError(ErrMsg)
-            length = len(matrix[0])
-            if len(row) != length:
-                raise TypeError(ErrMsg2)
-            for element in row:
-                if not isinstance(element, (int, float)):
-                    raise TypeError(ErrMsg)
-    else:
-        raise TypeError(ErrMsg)
+
     new_matrix = []
     for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(round((element / div), 2))
+        new_row = [round(element / div, 2) for element in row]
         new_matrix.append(new_row)
     return new_matrix
