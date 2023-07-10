4762ed73f9b4840277ae902baaa8a3e4e0757eaf
Sumshi <sumayasomow@gmail.com>
2023-07-10 04:21:51 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Write a class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """creates a class"""
+    def area(self):
+        """takes self"""
+        raise Exception("area() is not implemented")
