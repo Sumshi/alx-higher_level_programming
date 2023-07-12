1c2b78b57d4fc7ef72346e2196352a25f7c00d13
Sumshi <sumayasomow@gmail.com>
2023-07-12 07:03:30 +0000

    append before

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-"""Log Parsing Module."""
-import sys
-import contextlib
+
+"""
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
 
-def print_stats(size: int, status_codes: dict):
-    """Prints the metrics.
-
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
     Args:
-        size (int): The total read file size so far.
-        status_codes (dict): The status codes.
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
-    print(f"File size: {size}")
+    print("File size: {}".format(size))
     for key in sorted(status_codes):
-        print(f"{key}: {status_codes[key]}")
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
+    import sys
 
     size = 0
-    count = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
         for line in sys.stdin:
@@ -33,16 +37,22 @@ if __name__ == "__main__":
 
             line = line.split()
 
-            with contextlib.suppress(IndexError, ValueError):
+            try:
                 size += int(line[-1])
-            with contextlib.suppress(IndexError):
+            except (IndexError, ValueError):
+                pass
+
+            try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
                         status_codes[line[-2]] = 1
                     else:
                         status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
         print_stats(size, status_codes)
 
-    except KeyboardInterrupt as k:
+    except KeyboardInterrupt:
         print_stats(size, status_codes)
-        raise k
+        raise
