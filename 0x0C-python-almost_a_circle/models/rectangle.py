5b2594ccdc9a579fd39c20a0fb1f88132bc5ae51
Sumshi <sumayasomow@gmail.com>
2023-07-14 19:30:39 +0000

    added tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,7 +82,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """prints string representation of rectangle"""
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
 
     def update(self, *args, **kwargs):
         """assigns an argument to each attribute"""
