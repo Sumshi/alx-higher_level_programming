4b74bd766cca7d3abb22dd170da5a167752b489c
Sumshi <sumayasomow@gmail.com>
2023-07-17 17:38:13 +0000

    rectangle tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,6 @@
 
 """Defines unittests for models/rectangle.py."""
 
-
 import io
 import sys
 import unittest
@@ -14,75 +13,61 @@ class TestRectangle_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Rectangle class."""
 
     def test_rectangle_is_base(self):
-        """checks whether rectangle is an instance of base class"""
-        self.assertIsInstance(Rectangle(10, 5), Base)
+        self.assertIsInstance(Rectangle(10, 2), Base)
 
     def test_no_args(self):
-        """checks if error will be raised if passed no args"""
         with self.assertRaises(TypeError):
             Rectangle()
 
     def test_one_arg(self):
-        """checks if error will be raised if passed 1 args"""
         with self.assertRaises(TypeError):
             Rectangle(1)
 
     def test_two_args(self):
-        """checks if id attribute has been assingned correctly"""
-        r1 = Rectangle(10, 5)
-        r2 = Rectangle(5, 10)
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_three_args(self):
-        """checks if id attribute has been assingned correctly"""
-        r1 = Rectangle(20, 20, 40)
-        r2 = Rectangle(40, 40, 20)
+        r1 = Rectangle(2, 2, 4)
+        r2 = Rectangle(4, 4, 2)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_four_args(self):
-        """checks if id attribute has been assingned correctly"""
         r1 = Rectangle(1, 2, 3, 4)
         r2 = Rectangle(4, 3, 2, 1)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_five_args(self):
-        """checks if id attribute has been assingned correctly"""
-        self.assertEqual(20, Rectangle(10, 5, 0, 0, 20).id)
+        self.assertEqual(7, Rectangle(10, 2, 0, 0, 7).id)
 
     def test_more_than_five_args(self):
-        """rectangle has only id,width,height,x,y"""
         with self.assertRaises(TypeError):
             Rectangle(1, 2, 3, 4, 5, 6)
 
     def test_width_private(self):
-        """checks whether Rectangle has private attribute named _width"""
         with self.assertRaises(AttributeError):
-            print(Rectangle(4, 5, 0, 0, 1).__width)
+            print(Rectangle(5, 5, 0, 0, 1).__width)
 
     def test_height_private(self):
-        """checks whether Rectangle has private attribute named _height"""
         with self.assertRaises(AttributeError):
-            print(Rectangle(4, 5, 0, 0, 1).__height)
+            print(Rectangle(5, 5, 0, 0, 1).__height)
 
     def test_x_private(self):
-        """checks whether Rectangle has private attribute named __x"""
         with self.assertRaises(AttributeError):
-            print(Rectangle(4, 5, 0, 0, 1).__x)
+            print(Rectangle(5, 5, 0, 0, 1).__x)
 
     def test_y_private(self):
-        """checks whether Rectangle has private attribute named __y"""
         with self.assertRaises(AttributeError):
-            print(Rectangle(4, 5, 0, 0, 1).__y)
+            print(Rectangle(5, 5, 0, 0, 1).__y)
 
     def test_width_getter(self):
-        """ checks the getter method for the width attribute"""
-        r = Rectangle(6, 5, 4, 3, 2)
-        self.assertEqual(6, r.width)
+        r = Rectangle(5, 7, 7, 5, 1)
+        self.assertEqual(5, r.width)
 
     def test_width_setter(self):
-        """checks the setter method for the width attribute """
-        r = Rectangle(6, 5, 4, 3, 2)
-        r.width = 10  # updates the value of width to 10
+        r = Rectangle(5, 7, 7, 5, 1)
+        r.width = 10
         self.assertEqual(10, r.width)
 
     def test_height_getter(self):
@@ -804,7 +789,6 @@ class TestRectangle_to_dictionary(unittest.TestCase):
         r = Rectangle(10, 2, 4, 1, 2)
         with self.assertRaises(TypeError):
             r.to_dictionary(1)
-
 
 if __name__ == "__main__":
     unittest.main()
