9b0230c8583745268e3b9c3e06000521207a531f
Sumshi <sumayasomow@gmail.com>
2023-08-15 05:53:42 -0700

    displays in descending order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table
+-- Records should be ordered by score (top first)
+-- order is by descending
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
