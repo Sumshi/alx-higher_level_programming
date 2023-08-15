eaf82394c996dea5a77af7be80eb52b595fe6da3
Sumshi <sumayasomow@gmail.com>
2023-08-15 02:06:32 -0700

    task 4 getting unknown database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- script that creates a table called first_table
-
+-- If the table first_table already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
