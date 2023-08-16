c63cf74a71480d156fd6550974eec1b83bbe992a
Sumshi <sumayasomow@gmail.com>
2023-08-16 09:37:16 +0000

    assigns a default value

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- creates the table id_not_null on your MySQL server
+-- with description id and name
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256)
+);
