949a61c2f8e051d1ee6ba467216a8ebb339c70a1
Sumshi <sumayasomow@gmail.com>
2023-08-16 08:54:12 +0000

    lists all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- Create the user if it doesn't exist
--- Grant all privileges to the user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
