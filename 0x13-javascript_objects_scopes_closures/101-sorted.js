fe81d8e9796d5c18c95c2e0b4b8ec1d315b64040
Sumshi <sumayasomow@gmail.com>
2023-09-12 09:29:24 +0000

    dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,9 +5,8 @@ const newDict = {};
 
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = (key);
-  } else {
-    newDict[dict[key]].push(key);
+    newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(key);
 }
 console.log(newDict);
