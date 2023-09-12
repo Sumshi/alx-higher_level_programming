db4394eed83b77a55f1c717670b6eba40f14f0c9
Sumshi <sumayasomow@gmail.com>
2023-09-12 09:46:43 +0000

    last task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,34 @@
 #!/usr/bin/node
-// concats 2 files.
 const fs = require('fs');
-const src1 = fs.readFileSync(process.argv[2], 'utf8');
-const src2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], src1 + src2);
+
+if (process.argv.length !== 5) {
+  console.error('Usage: node 102-concat.js <fileA> <fileB> <fileC>');
+  process.exit(1);
+}
+
+const [, , fileA, fileB, fileC] = process.argv;
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) {
+    console.error(`Error reading ${fileA}: ${err.message}`);
+    process.exit(1);
+  }
+
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) {
+      console.error(`Error reading ${fileB}: ${err.message}`);
+      process.exit(1);
+    }
+
+    const concatenatedData = dataA + '\n' + dataB;
+
+    fs.writeFile(fileC, concatenatedData, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to ${fileC}: ${err.message}`);
+        process.exit(1);
+      }
+
+      console.log(`Concatenated ${fileA} and ${fileB} into ${fileC}`);
+    });
+  });
+});
