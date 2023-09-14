e34ed301633e72c528c0229e1c40f6d1795a3640
Sumshi <sumayasomow@gmail.com>
2023-09-14 16:25:58 +0000

    session

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Module that connects python script to a database
+and retrieves data from table states and prints
+id and name
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker  # creates session interacting with db
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True  # specifies that the engine should check db status
+    )
+
+    # create session maker object that binds to the previous db
+    my_session_maker = sessionmaker(bind=engine)
+
+    # creates a new session
+    my_session = my_session_maker()
+
+    for state in my_session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+
+    # close the session
+    my_session.close()
