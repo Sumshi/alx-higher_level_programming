47da629c19093f3d03a68ab5d55eee11542f0d65
Sumshi <sumayasomow@gmail.com>
2023-09-15 06:06:16 +0000

    cheking tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+Module that connects a python script to a database
 """
 
-# code should not be executed when imported
 if __name__ == "__main__":
+
     import MySQLdb
     from sys import argv
 
-# connect to mysqldb server
-my_database = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    database=argv[3],
-    port=3306
-)
+    # Connect database using command-line arguments
+    my_database = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    # define cursor used to execute mysql queries
+    my_cursor = my_database.cursor()
 
-# define cursor used to execute mysql queries
-my_cursor = my_database.cursor()
+    # Execute a SELECT query to select data
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id;"
+    my_cursor.execute(query, (argv[4],))
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
 
-# execute a select query to select data
-query = "SELECT * FROM states WHERE name = %s ORDER BY states.id;"
-my_cursor.execute(query, (argv[4],))
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
 
-# fetch the data from the query in tuple form
-my_data = my_cursor.fetchall()
+    # Close all cursors
+    my_cursor.close()
 
-# iterate through the fetched data and print each row
-for row in my_data:
-    print(row)
-
-# finally close the cursor
-my_cursor.close()
+    # Close all databases
+    my_database.close()
