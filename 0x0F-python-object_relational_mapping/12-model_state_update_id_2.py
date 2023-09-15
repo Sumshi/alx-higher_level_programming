a1ca2b9b8b037d763288db99f3dfb2c63ea03c08
Sumshi <sumayasomow@gmail.com>
2023-09-15 14:07:44 +0000

    updates a query

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Module that connects python script to a database
+and retrieves data from table states and prints
+id and name
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True  # specifies that the engine should check db status
+    )
+
+    # create session maker object that binds to the previous db
+    Session = sessionmaker(bind=engine)
+
+    # creates a new session
+    my_session = Session()
+
+    state = my_session.get(State, 2)
+    # state = my_session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    my_session.add(state)
+    my_session.commit()
+    # close the session
+    my_session.close()
