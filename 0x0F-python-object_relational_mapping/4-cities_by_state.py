1a053410e9d502e9f57b6908a11520ef4621709f
Sumshi <sumayasomow@gmail.com>
2023-09-15 06:12:18 +0000

    cheking tasks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+Module that connects a python script to a database
 """
 
-# code should not be executed when imported
 if __name__ == "__main__":
+
     import MySQLdb
     from sys import argv
 
-# connect to mysqldb server
-my_database = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    database=argv[3],
-    port=3306
-)
+    # Connect database using command-line arguments
+    my_database = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    # define cursor used to execute mysql queries
+    my_cursor = my_database.cursor()
 
-# define cursor used to execute mysql queries
-my_cursor = my_database.cursor()
-
-# execute a select query to select data
-my_cursor.execute(
-    """SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states
-    ON states.id = cities.state_id
-    ORDER BY cities.id
+    # Execute a SELECT query to select data
+    my_cursor.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states
+        ON states.id = cities.state_id
+        ORDER BY cities.id
     """
-)
+    )
 
-# fetch the data from the query in tuple form
-my_data = my_cursor.fetchall()
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
 
-# iterate through the fetched data and print each row
-for row in my_data:
-    print(row)
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
 
-# finally close the cursor
-my_cursor.close()
+    # Close all cursors
+    my_cursor.close()
+
+    # Close all databases
+    my_database.close()
