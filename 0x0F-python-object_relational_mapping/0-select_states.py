3245abd9e1ecd09674060aaa8191ffa7ebf4c9c3
Sumshi <sumayasomow@gmail.com>
2023-09-15 05:47:29 +0000

    cheking task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,32 +8,29 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    # Connect database to mysql server
-    # connect to mysqldb server
-my_db = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    database=argv[3],
-    port=3306
-)
-# define cursor used to execute mysql queries
-my_cursor = my_db.cursor()
+    # Connect database using command-line arguments
+    my_database = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
+    # Create cursor obj to interact with database
+    my_cursor = my_database.cursor()
 
-# Execute a SELECT query to select data
-my_cursor.execute(
-        "SELECT * FROM states ORDER BY states.id ASC;"
-)
+    # Execute a SELECT query to fetch data
+    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-# fetch all the data returned by the query
-my_data = my_cursor.fetchall()
+    # fetch all the data returned by the query
+    my_data = my_cursor.fetchall()
 
-# Iterate through the fetched data and print each row
-for row in my_data:
-    print(row)
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
 
-# Close all cursors
-my_cursor.close()
+    # Close all cursors
+    my_cursor.close()
 
-# Close all databases
-my_db.close()
+    # Close all databases
+    my_database.close()
