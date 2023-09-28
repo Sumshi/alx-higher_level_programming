6e4bda57f5cab4a025d8c478a4cd05d34ab313e9
Sumshi <sumayasomow@gmail.com>
2023-09-28 05:26:33 +0000

    status code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # takes in a url and sends it a reguest
 # sends a get reguest to the url
-curl -s "$1" | wc -c
+#curl -s "$1" | wc -c
+
+URL=$1 # first command line argument
+#get reguest to the url provided
+response=$(curl -sI "$URL") # -s for silent mode, -I fetches only response headers and not body
+content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
+echo "$content_length"
+
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+URL=$1
+response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+if [ "$response" -eq 200 ]; then
+    curl -s "$URL"
+else
+    echo "Error: Response status code was $response"
+fi
