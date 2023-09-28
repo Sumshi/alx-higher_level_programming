0d36b53c76a0def5e22f40638ba4089b1ee2951e
Sumshi <sumayasomow@gmail.com>
2023-09-28 09:15:04 +0000

    added requirements

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 #takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-URL=$1
-response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-if [ "$response" -eq 200 ]; then
-    curl -s "$URL"
-fi
-
