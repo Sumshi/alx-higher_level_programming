a70bbbf9dd1093b203352b19c1f402ffc14c6219
Sumshi <sumayasomow@gmail.com>
2023-09-29 08:00:17 +0000

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,18 +2,20 @@
 
 
 """ script that fetches url"""
-import urllib.request
-import urllib.parse
-import sys
+import urllib.request  # for making http requests
+import urllib.parse  # for url encoding
+import sys  # for command line arguments
 
 if __name__ == "__main__":
     url = sys.argv[1]  # url from command line
     email = sys.argv[2]
     data = {'email': email}
+    # encodes the data dictionary
     data = urllib.parse.urlencode(data).encode('utf-8')
-    # creating a post request
+    # creating a post request using Request class
     request = urllib.request.Request(url, data)
     # handle the response
     with urllib.request.urlopen(request) as response:
+        # read the response and retrieves body as bytes
         body = response.read().decode('utf-8')
         print("Your email is:", email)
