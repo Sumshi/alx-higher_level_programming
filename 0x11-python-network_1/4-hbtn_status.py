fc8436bdd93ca34bb11c465ef5788f53d7d54da7
Sumshi <sumayasomow@gmail.com>
2023-09-29 08:49:06 +0000

    usage the package requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""  script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    data = response.text
+    print(data)
