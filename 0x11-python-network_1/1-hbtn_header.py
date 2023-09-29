d2446fc59563e65270c13a297ef0a0428a81bc6d
Sumshi <sumayasomow@gmail.com>
2023-09-29 06:18:54 +0000

    gets a header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,5 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print(" - type:", type(body))
-        print(" - content:", body)
-        print(" - utf8 content:", html.decode('utf-8'))
+        header_value = response.getheader('X-Request-Id')
+        print(header_value)
