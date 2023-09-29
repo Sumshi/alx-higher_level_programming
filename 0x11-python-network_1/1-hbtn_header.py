79caa360da2304e61e9a2b4ea9ff6699566e606c
Sumshi <sumayasomow@gmail.com>
2023-09-29 06:03:32 +0000

    fetches a url

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(" - type:", type(body))
+        print(" - content:", body)
+        print(" - utf8 content:", html.decode('utf-8'))
