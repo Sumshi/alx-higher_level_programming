f790481b17a2ddc9a580c1e557d17a9074e69cb7
Sumshi <sumayasomow@gmail.com>
2023-09-29 07:08:15 +0000

    post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+""" script that fetches url"""
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # url from command line
+    email = sys.argv[2]
+    with urllib.request.urlopen(url/email) as response:
+        body = response.read()
+        print("Your email is:", email)
