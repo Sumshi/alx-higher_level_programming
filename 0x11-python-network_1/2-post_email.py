88328a8c877130a8aa45abee14d03b27dd43660c
Sumshi <sumayasomow@gmail.com>
2023-09-29 07:19:27 +0000

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,11 +3,17 @@
 
 """ script that fetches url"""
 import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]  # url from command line
     email = sys.argv[2]
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+    data = {'email': email}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    # creating a post request
+    request = urllib.request.Request(url, data)
+    # handle the response
+    with urllib.request.urlopen(request) as response:
+        body = response.read().decode('utf-8')
         print("Your email is:", email)
