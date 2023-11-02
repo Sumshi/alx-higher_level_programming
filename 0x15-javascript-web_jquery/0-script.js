ca5910c7967aa4eeb81e1a1392f9a3a6f5713c35
Sumshi <sumayasomow@gmail.com>
2023-11-02 06:49:05 +0000

    done

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,4 +1,4 @@
-// check if header exists and is present
+// check if header exists and is present and change color
 const header = document.querySelector('header');
 if (header) {
   header.style.color = '#FF0000';
